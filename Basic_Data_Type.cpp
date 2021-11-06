169176bc8c2b8ab31b077d4586fc734640bfea54
akram.hl19 <edvinkarlsvensson@gmail.com>
2021-11-06 21:43:43 +0100

    B

---
+++ Basic_Data_Type.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+char ch;
+int nomb;
+double b,d;
+long long l;
+ 
+    cin >> nomb >> l >> ch >> d >> b;
+    cout << nomb << endl << l << endl << ch << endl << d <<endl;
+    cout << fixed <<setprecision(1) << b;
+}
